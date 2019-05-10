7106047a25257826a4771805669258f16b9fd5a4
洪剑宏 <565983236@qq.com>
2019-05-10 08:43:54 +0800

    update files 2019年 5月10日 星期五 08时43分54秒 CST

--- services/user_service.go
+++ services/user_service.go
@@ -10,8 +10,32 @@ import (
 // UserService 用户服务
 type UserService struct{}
 
-// Store implements helloworld.GreeterServer
+// Index 用户列表
+func (s *UserService) Index(ctx context.Context, in *pb.UserRequest) (*pb.UserReply, error) {
+	log.Printf("Received: %v", in.Username)
+	return &pb.UserReply{Message: "1XX " + in.Username}, nil
+}
+
+// Store 用户添加
 func (s *UserService) Store(ctx context.Context, in *pb.UserRequest) (*pb.UserReply, error) {
+	log.Printf("Received: %v", in.Username)
+	return &pb.UserReply{Message: "1XX " + in.Username}, nil
+}
+
+// Show ...
+func (s *UserService) Show(ctx context.Context, in *pb.UserRequest) (*pb.UserReply, error) {
+	log.Printf("Received: %v", in.Username)
+	return &pb.UserReply{Message: "1XX " + in.Username}, nil
+}
+
+// Update ...
+func (s *UserService) Update(ctx context.Context, in *pb.UserRequest) (*pb.UserReply, error) {
+	log.Printf("Received: %v", in.Username)
+	return &pb.UserReply{Message: "1XX " + in.Username}, nil
+}
+
+// Destroy ...
+func (s *UserService) Destroy(ctx context.Context, in *pb.UserRequest) (*pb.UserReply, error) {
 	log.Printf("Received: %v", in.Username)
 	return &pb.UserReply{Message: "1XX " + in.Username}, nil
 }
